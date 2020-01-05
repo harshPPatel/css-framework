cb2a6c3387fa34056bbb6dbe88e32841d08e8e6d
aguin467 <a.guin003@gmail.com>
2020-01-05 15:23:34 -0500

    Added the js code and code to the html

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,1 +1,11 @@
-console.log('hello from alert.js');
+let textArea = document.getElementById('text-area');
+let okButton = document.getElementById('ok-button');
+let paragraph = document.getElementById('text-area');
+
+okButton.addEventListener('click', function() {
+    textArea.innerText = 'Button clicked!';
+});
+
+//console.log('hello from alert.js');
+
+
